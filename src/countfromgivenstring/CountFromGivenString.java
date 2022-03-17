a5e11d58960598921b631bb7eeb9fdf6ebc74c49
Dusto7172 <rockyrock7125@gmail.com>
2022-03-17 15:18:56 +0530

    CountFromGivenString

--- src/countfromgivenstring/CountFromGivenString.java
+++ src/countfromgivenstring/CountFromGivenString.java
@@ -10,6 +10,7 @@ public static void main(String[] args) {
 	
 	String[]s2=s1.split("");
 	System.out.println(Arrays.toString(s2));
+	
 	System.out.printf("%-10s%s \n", "String", "Count");
 	
 	for(int i =0;i<s2.length;i++){
